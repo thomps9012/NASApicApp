fc3f50960e814b457bac7de96653c2c6b7b207bd
Samuel Thompson <thomps9012@gmail.com>
2020-12-08 16:02:28 -0500

    removed config.json from gitignore

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -34,7 +34,6 @@ export default function NasaPhoto() {
                         className="photo"
                         src={photoData.url}
                         alt={photoData.title}
-                        className="photo"
                     />
                 ) : (
                         <iframe
